2aeb55b03788f7d831a0e81c5b78f706493feaee
Dzmitry Charnamortsau <d.charnamortsau@gmail.com>
2025-12-15 11:32:38 -0800

    created a database and connected it to the app

---
+++ src/main/java/org/yearup/GroceryApp.java
@@ -0,0 +1,14 @@
+package org.yearup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryApp
+{
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroceryApp.class, args);
+    }
+
+}
